1b19e4d3e87fbd5053c9fa34d0c0fe298e3464e1
mpollayil <mathewjosepollayil@gmail.com>
2019-02-27 17:43:14 +0100

    Added the slerp controller and temporarily added it to adaptive grasp controlller in cmake

---
+++ include/SlerpControl.h
@@ -0,0 +1,83 @@
+/* SLERP CONTROLLER - Uses SLERP interpolation between presnt ee pose and goal pose to create homogeneous motion
+Authors: George Jose Pollayil - Mathew Jose Pollayil
+Email: gpollayil@gmail.com, mathewjosepollayil@gmail.com  */
+
+// Basic includes
+#include <ros/service.h>
+#include <Eigen/Dense>
+#include <eigen_conversions/eigen_msg.h>
+#include <tf/transform_listener.h>
+#include <tf_conversions/tf_eigen.h>
+
+// ROS msg includes
+#include <geometry_msgs/Pose.h>
+#include <trajectory_msgs/JointTrajectory.h>
+#include <control_msgs/FollowJointTrajectoryAction.h>
+
+// Custom msg and srv includes
+#include "adaptive_grasp_controller/slerp_control.h"
+
+// MoveIt! includes
+#include <moveit/move_group_interface/move_group_interface.h>
+
+// ROS action includes
+#include <actionlib/client/simple_action_client.h>
+#include <actionlib/client/terminal_state.h>
+
+// Defines
+#define     DEBUG   1       // Prints out additional stuff
+
+class SlerpControl {
+
+    /// public variables and functions ------------------------------------------------------------
+	public:
+		SlerpControl(ros::NodeHandle& nh_, std::string group_name_, std::string end_effector_name_, int n_wp_,
+            boost::shared_ptr<actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction>> arm_client_ptr_);
+
+        ~SlerpControl();
+
+        // This is the callback function of the slerp control service
+	  	bool call_slerp_control(adaptive_grasp_controller::slerp_control::Request &req, adaptive_grasp_controller::slerp_control::Response &res);
+
+	  	// Initialize the things for motion planning. Is called by the callback
+	  	bool initialize(geometry_msgs::Pose goal_pose, bool is_goal_relative);
+
+		// Performs motion planning for the end-effector towards goal
+		bool performMotionPlan();
+
+		// Computes waypoints using SLERP from two poses
+		void computeWaypointsFromPoses(const Eigen::Affine3d& start_pose, const Eigen::Affine3d& goal_pose, std::vector<geometry_msgs::Pose>& waypoints);
+
+		// Sends trajectory to the joint_traj controller
+		bool sendJointTrajectory(trajectory_msgs::JointTrajectory trajectory);
+
+	/// private variables -------------------------------------------------------------------------
+	private:
+		ros::NodeHandle nh;
+
+        // Important names
+        std::string end_effector_name;                          // Name of the end-effector link
+        std::string group_name;                                 // Name of the MoveIt group
+
+        // Number of waypoints for slerp interpolation
+        int n_wp;
+
+        // The arm action client
+        boost::shared_ptr<actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction>> arm_client_ptr;
+
+        // Move group interface
+        moveit::planning_interface::MoveGroupInterface group;
+
+        // Tf listener and transform and the tmp eigen
+	    tf::TransformListener tf_listener;
+        tf::StampedTransform stamp_ee_transform;
+        Eigen::Affine3d end_effector_state;
+
+        // The start and goal poses of the control
+	  	Eigen::Affine3d startAff; 								// The starting ee pose 
+	  	Eigen::Affine3d goalAff; 								// The goal pose given by service call
+
+        // Joint trajectory computed to be sent to robot
+        trajectory_msgs::JointTrajectory computed_trajectory;  
+	
+};
---
+++ src/SlerpControl.cpp
@@ -0,0 +1,176 @@
+/* KUKA SLERP CONTROLLER - Uses SLERP interpolation between ee pose and goal pose to create homogeneous motion
+Authors: George Jose Pollayil - Mathew Jose Pollayil
+Email: gpollayil@gmail.com, mathewjosepollayil@gmail.com  */
+
+#include "ros/ros.h"
+#include <cstdlib>
+#include <sstream>
+#include <string>
+
+#include "SlerpControl.h"
+
+SlerpControl::SlerpControl(ros::NodeHandle& nh_, std::string group_name_, std::string end_effector_name_,  int n_wp_,
+    boost::shared_ptr<actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction>> arm_client_ptr_) : group(group_name) {
+        
+        // Initializing node handle
+        this->nh = nh_;
+
+        // Initializing names
+        this->end_effector_name = end_effector_name_;
+        this->group_name = group_name_;
+
+        // Setting number of waypoints 
+        this->n_wp = n_wp_;
+
+        // Initializing the arm client
+        this->arm_client_ptr = arm_client_ptr_;
+    }
+
+SlerpControl::~SlerpControl(){
+    
+    // Nothing to do here yet
+}
+
+// This is the callback function of the slerp control service
+bool SlerpControl::call_slerp_control(adaptive_grasp_controller::slerp_control::Request &req, adaptive_grasp_controller::slerp_control::Response &res){
+
+    // Setting up things
+    if(!this->initialize(req.goal_pose, req.is_goal_relative)){
+        res.answer = false;
+        return false;
+    }
+
+	// Perform motion plan towards the goal pose
+    if(!this->performMotionPlan()){
+        res.answer = false;
+        return false;
+    }
+
+    // Send computed joint motion
+    if(!this->sendJointTrajectory(this->computed_trajectory)){
+        res.answer = false;
+        return false;
+    }
+
+    // At this point everything is completed, return true
+    res.answer = true;
+    return true;
+}
+
+
+// Initialize the things for motion planning. Is called by the callback
+bool SlerpControl::initialize(geometry_msgs::Pose goal_pose, bool is_goal_relative){
+
+    // Getting the current ee transform
+    try {
+		this->tf_listener.waitForTransform("/world", this->end_effector_name, ros::Time(0), ros::Duration(10.0) );
+		this->tf_listener.lookupTransform("/world", this->end_effector_name, ros::Time(0), this->stamp_ee_transform);
+    } catch (tf::TransformException ex){
+      	ROS_ERROR("%s", ex.what());
+      	ros::Duration(1.0).sleep();
+        return false;
+    }
+
+    tf::Transform ee_transform(this->stamp_ee_transform.getRotation(), this->stamp_ee_transform.getOrigin());
+    tf::transformTFToEigen(ee_transform, this->end_effector_state);
+
+	// Print the current end-effector pose
+	if(DEBUG) ROS_INFO_STREAM("Endeffector current Translation: " << this->end_effector_state.translation());
+	if(DEBUG) ROS_INFO_STREAM("Endeffector current Rotation: " << this->end_effector_state.rotation());
+
+	// Setting the start pose 
+	this->startAff = this->end_effector_state;
+
+	// Setting the goal pose
+    tf::poseMsgToEigen(goal_pose, this->goalAff);
+
+	// If the goal is relative, get the global goal pose by multiplying it with ee pose (end_effector_state)
+	if(is_goal_relative){
+		this->goalAff = this->end_effector_state * this->goalAff;
+	}
+
+    // Print the goal end-effector pose
+    if(DEBUG) ROS_INFO_STREAM("Endeffector goal Translation: " << this->goalAff.translation());
+	if(DEBUG) ROS_INFO_STREAM("Endeffector goal Rotation: " << this->goalAff.linear());
+
+    return true;
+}
+
+// Performs motion planning for the end-effector towards goal
+bool SlerpControl::performMotionPlan(){
+
+	// Printing the planning group frame and the group ee frame
+	if(DEBUG) ROS_INFO("MoveIt Group Reference frame: %s", this->group.getPlanningFrame().c_str());
+	if(DEBUG) ROS_INFO("MoveIt Group End-effector frame: %s", this->group.getEndEffectorLink().c_str());
+
+	// Calling the waypoint creator with start and goal poses
+	std::vector<geometry_msgs::Pose> cart_waypoints;
+	this->computeWaypointsFromPoses(this->startAff, this->goalAff, cart_waypoints);
+
+	// Planning for the waypoints path
+	moveit_msgs::RobotTrajectory trajectory;
+	double fraction = this->group.computeCartesianPath(cart_waypoints, 0.01, 0.0, trajectory);
+    this->computed_trajectory = trajectory.joint_trajectory;
+
+	ROS_INFO("Plan (cartesian path) (%.2f%% acheived)", fraction * 100.0);
+
+    // If complete path is not achieved return false, true otherwise
+	if(fraction != 1.0) return false;
+
+    std::cout << "FOUND COMPLETE PLAN FOR WAYPOINTS!!!" << std::endl;
+
+    return true;
+}
+
+// Computes waypoints using SLERP from two poses
+void SlerpControl::computeWaypointsFromPoses(const Eigen::Affine3d& start_pose, const Eigen::Affine3d& goal_pose, std::vector<geometry_msgs::Pose>& waypoints){
+    
+    // Compute waypoints as linear interpolation (SLERP for rotaions) between the two poses
+	Eigen::Affine3d wp_eigen;
+	geometry_msgs::Pose current_wp;
+
+	Eigen::Vector3d start_vec = start_pose.translation();
+	Eigen::Vector3d goal_vec = goal_pose.translation();
+	Eigen::Vector3d diff_vec = goal_vec - start_vec;
+
+	Eigen::Quaterniond start_quat(start_pose.linear());
+	Eigen::Quaterniond goal_quat(goal_pose.linear());
+	Eigen::Quaterniond diff_quat;
+
+	for(int j = 1; j <= this->n_wp; j++){
+		wp_eigen.translation() = start_vec + (diff_vec / this->n_wp) * j;
+		diff_quat = start_quat.slerp(double(j)/double(this->n_wp), goal_quat);
+		wp_eigen.linear() = diff_quat.toRotationMatrix();
+		tf::poseEigenToMsg (wp_eigen, current_wp);
+		waypoints.push_back(current_wp);
+
+        if(DEBUG){
+            std::cout << "WAYPOINT NUMBER " << j << "." << std::endl;
+		    std::cout << "WP R: " << wp_eigen.linear() << std::endl;
+		    std::cout << "WP t: " << wp_eigen.translation() << std::endl;
+        }
+    }
+}
+
+// Sends trajectory to the joint_traj controller
+bool SlerpControl::sendJointTrajectory(trajectory_msgs::JointTrajectory trajectory){
+    
+    // Waiting for the arm server to be ready
+    if(!this->arm_client_ptr->waitForServer(ros::Duration(1,0))){
+        ROS_ERROR("The arm client is taking too much to get ready. Returning...");
+        return false;
+    }
+
+	// Send the message and wait for the result
+	control_msgs::FollowJointTrajectoryGoal goalmsg;
+	goalmsg.trajectory = trajectory;
+
+    this->arm_client_ptr->sendGoal(goalmsg);
+
+    if(!this->arm_client_ptr->waitForResult(ros::Duration(20, 0))){
+        ROS_ERROR("The arm client is taking too to complete goal execution. Returning...");
+        return false;
+    }
+
+    return true;
+}
