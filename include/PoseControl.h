16f0b5d20a82d4e2eeefd2005ee7048693d56bcd
mpollayil <mathewjosepollayil@gmail.com>
2019-02-28 15:33:06 +0100

    Adding the planning to pose with moveit

---
+++ include/PoseControl.h
@@ -0,0 +1,74 @@
+/* POSE CONTROL - Uses moveit movegroupinterface to plan towards a pose
+Authors: George Jose Pollayil - Mathew Jose Pollayil
+Email: gpollayil@gmail.com, mathewjosepollayil@gmail.com  */
+
+// Basic includes
+#include <ros/service.h>
+#include <Eigen/Dense>
+#include <eigen_conversions/eigen_msg.h>
+#include <tf/transform_listener.h>
+#include <tf_conversions/tf_eigen.h>
+
+// ROS msg includes
+#include <geometry_msgs/Pose.h>
+#include <trajectory_msgs/JointTrajectory.h>
+#include <control_msgs/FollowJointTrajectoryAction.h>
+
+// Custom msg and srv includes
+#include "adaptive_grasp_controller/pose_control.h"
+
+// MoveIt! includes
+#include <moveit/move_group_interface/move_group_interface.h>
+
+// ROS action includes
+#include <actionlib/client/simple_action_client.h>
+#include <actionlib/client/terminal_state.h>
+
+// Defines
+#define     DEBUG   1       // Prints out additional stuff
+
+class PoseControl {
+
+    /// public variables and functions ------------------------------------------------------------
+	public:
+		PoseControl(ros::NodeHandle& nh_, std::string group_name_, std::string end_effector_name_, int n_wp_,
+            boost::shared_ptr<actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction>> arm_client_ptr_);
+
+        ~PoseControl();
+
+        // This is the callback function of the pose control service
+	  	bool call_pose_control(adaptive_grasp_controller::pose_control::Request &req, adaptive_grasp_controller::pose_control::Response &res);
+
+	  	// Initialize the things for motion planning. It is called by the callback
+	  	bool initialize(geometry_msgs::Pose goal_pose, bool is_goal_relative);
+
+		// Performs motion planning for the end-effector towards goal
+		bool performMotionPlan();
+
+		// Sends trajectory to the joint_traj controller
+		bool sendJointTrajectory(trajectory_msgs::JointTrajectory trajectory);
+
+	/// private variables -------------------------------------------------------------------------
+	private:
+		ros::NodeHandle nh;
+
+        // Important names
+        std::string end_effector_name;                          // Name of the end-effector link
+        std::string group_name;                                 // Name of the MoveIt group
+
+        // The arm action client
+        boost::shared_ptr<actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction>> arm_client_ptr;
+
+        // Tf listener and transform and the tmp eigen
+	    tf::TransformListener tf_listener;
+        tf::StampedTransform stamp_ee_transform;
+        Eigen::Affine3d end_effector_state;
+
+        // The goal pose
+	  	geometry_msgs::Pose goalPose; 								// The goal pose given by service call
+        Eigen::Affine3d goalPoseAff;                                // The same as Eigen Affine
+
+        // Joint trajectory computed to be sent to robot
+        trajectory_msgs::JointTrajectory computed_trajectory;  
+	
+};
---
+++ src/PoseControl.cpp
@@ -0,0 +1,151 @@
+/* POSE CONTROL - Uses moveit movegroupinterface to plan towards a pose
+Authors: George Jose Pollayil - Mathew Jose Pollayil
+Email: gpollayil@gmail.com, mathewjosepollayil@gmail.com  */
+
+#include "ros/ros.h"
+#include <cstdlib>
+#include <sstream>
+#include <string>
+
+#include "PoseControl.h"
+
+#include <moveit_visual_tools/moveit_visual_tools.h>
+
+PoseControl::PoseControl(ros::NodeHandle& nh_, std::string group_name_, std::string end_effector_name_,  int n_wp_,
+    boost::shared_ptr<actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction>> arm_client_ptr_){
+        
+        ROS_INFO("Starting to create PoseControl object");
+
+        // Initializing node handle
+        this->nh = nh_;
+
+        // Initializing names
+        this->end_effector_name = end_effector_name_;
+        this->group_name = group_name_;
+
+        // Initializing the arm client
+        this->arm_client_ptr = arm_client_ptr_;
+
+        ROS_INFO("Finished creating PoseControl object");
+}
+
+PoseControl::~PoseControl(){
+    
+    // Nothing to do here yet
+}
+
+// This is the callback function of the pose control service
+bool PoseControl::call_pose_control(adaptive_grasp_controller::pose_control::Request &req, adaptive_grasp_controller::pose_control::Response &res){
+
+    // Setting up things
+    if(!this->initialize(req.goal_pose, req.is_goal_relative)){
+        ROS_ERROR("Could not initialize PoseControl object. Returning...");
+        res.answer = false;
+        return false;
+    }
+
+	// Perform motion plan towards the goal pose
+    if(!this->performMotionPlan()){
+        ROS_ERROR("Could not perform motion planning in PoseControl object. Returning...");
+        res.answer = false;
+        return false;
+    }
+
+    // Send computed joint motion
+    if(!this->sendJointTrajectory(this->computed_trajectory)){
+        ROS_ERROR("Could not send computed trajectory from PoseControl object. Returning...");
+        res.answer = false;
+        return false;
+    }
+
+    // At this point everything is completed, return true
+    res.answer = true;
+    return true;
+}
+
+
+// Initialize the things for motion planning. Is called by the callback
+bool PoseControl::initialize(geometry_msgs::Pose goal_pose, bool is_goal_relative){
+
+    // Getting the current ee transform
+    try {
+		this->tf_listener.waitForTransform("/world", this->end_effector_name, ros::Time(0), ros::Duration(10.0) );
+		this->tf_listener.lookupTransform("/world", this->end_effector_name, ros::Time(0), this->stamp_ee_transform);
+    } catch (tf::TransformException ex){
+      	ROS_ERROR("%s", ex.what());
+      	ros::Duration(1.0).sleep();
+        return false;
+    }
+
+    tf::Transform ee_transform(this->stamp_ee_transform.getRotation(), this->stamp_ee_transform.getOrigin());
+    tf::transformTFToEigen(ee_transform, this->end_effector_state);
+
+	// Print the current end-effector pose
+	if(DEBUG) ROS_INFO_STREAM("Endeffector current Translation: " << this->end_effector_state.translation());
+	if(DEBUG) ROS_INFO_STREAM("Endeffector current Rotation: " << this->end_effector_state.rotation());
+
+	// Setting the goal pose
+    tf::poseMsgToEigen(goal_pose, this->goalPoseAff);
+
+	// If the goal is relative, get the global goal pose by multiplying it with ee pose (end_effector_state)
+	if(is_goal_relative){
+		this->goalPoseAff = this->end_effector_state * this->goalPoseAff;
+	}
+
+    // Reconvert to geometry_msgs Pose
+    tf::poseEigenToMsg(this->goalPoseAff, this->goalPose);
+
+    // Print the goal end-effector pose
+    if(DEBUG) ROS_INFO_STREAM("Endeffector goal Translation: " << this->goalPoseAff.translation());
+	if(DEBUG) ROS_INFO_STREAM("Endeffector goal Rotation: " << this->goalPoseAff.linear());
+
+    return true;
+}
+
+// Performs motion planning for the end-effector towards goal
+bool PoseControl::performMotionPlan(){
+
+    // Move group interface
+    moveit::planning_interface::MoveGroupInterface group(this->group_name);
+
+	// Printing the planning group frame and the group ee frame
+	if(DEBUG) ROS_INFO("MoveIt Group Reference frame: %s", group.getPlanningFrame().c_str());
+	if(DEBUG) ROS_INFO("MoveIt Group End-effector frame: %s", group.getEndEffectorLink().c_str());
+
+    // Setting the pose target of the move group
+    group.setPoseTarget(this->goalPose);
+
+    // Planning to Pose
+    moveit::planning_interface::MoveGroupInterface::Plan plan;
+    bool success = (group.plan(plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+    ROS_INFO("Motion Plan towards goal pose %s.", success ? "SUCCEDED" : "FAILED");
+
+    // If complete path is not achieved return false, true otherwise
+	if(!success) return false;
+
+    // Saving the computed trajectory and returning true
+    this->computed_trajectory = plan.trajectory_.joint_trajectory;
+    return true;
+}
+
+// Sends trajectory to the joint_traj controller
+bool PoseControl::sendJointTrajectory(trajectory_msgs::JointTrajectory trajectory){
+    
+    // Waiting for the arm server to be ready
+    if(!this->arm_client_ptr->waitForServer(ros::Duration(1,0))){
+        ROS_ERROR("The arm client is taking too much to get ready. Returning...");
+        return false;
+    }
+
+	// Send the message and wait for the result
+	control_msgs::FollowJointTrajectoryGoal goalmsg;
+	goalmsg.trajectory = trajectory;
+
+    this->arm_client_ptr->sendGoal(goalmsg);
+
+    if(!this->arm_client_ptr->waitForResult(ros::Duration(30, 0))){
+        ROS_WARN("The arm client is taking too to complete goal execution. Is it a really long motion???");
+    }
+
+    return true;
+}
