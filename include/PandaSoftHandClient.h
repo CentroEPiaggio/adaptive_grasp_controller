2967f1b86821e87d4b55bbb385895e3c90b7c2f0
mpollayil <mathewjosepollayil@gmail.com>
2019-03-04 13:34:01 +0100

    Adding the client for the demo

---
+++ include/PandaSoftHandClient.h
@@ -0,0 +1,62 @@
+/* PANDA SOFTHAND CLIENT - Contains all necessary objects and functions to call the services to 
+control Panda + SoftHand
+Authors: George Jose Pollayil - Mathew Jose Pollayil
+Email: gpollayil@gmail.com, mathewjosepollayil@gmail.com  */
+
+// Basic includes
+#include <ros/service.h>
+
+// ROS msg includes
+#include <geometry_msgs/Pose.h>
+
+// Custom msg and srv includes
+#include "adaptive_grasp_controller/adaptive_control.h"
+#include "adaptive_grasp_controller/hand_control.h"
+#include "adaptive_grasp_controller/joint_control.h"
+#include "adaptive_grasp_controller/pose_control.h"
+#include "adaptive_grasp_controller/slerp_control.h"
+
+// Defines
+#define     DEBUG   1       // Prints out additional stuff
+#define     VISUAL          // Publishes visual info on RViz
+
+class PandaSoftHandClient {
+
+    /// public variables and functions ------------------------------------------------------------
+	public:
+		PandaSoftHandClient(ros::NodeHandle& nh_);
+
+        ~PandaSoftHandClient();
+
+        // Service call function for adaptive control
+        bool call_adaptive_service(bool run);
+
+        // Service call function for hand control
+        bool call_hand_service(double goal_syn, double goal_duration);
+
+        // Service call function for joint control
+        bool call_joint_service(std::vector<double> joint_goal);
+
+        // Service call function for pose control
+        bool call_pose_service(std::vector<double> pose_goal);
+
+
+	/// private variables -------------------------------------------------------------------------
+	private:
+		ros::NodeHandle nh;
+
+        // Service names
+        std::string adaptive_service_name;
+        std::string hand_service_name;
+        std::string joint_service_name;
+        std::string pose_service_name;
+        std::string slerp_service_name;
+
+        // Service clients
+        ros::ServiceClient adaptive_client;                 // Client for adaptive control service
+        ros::ServiceClient hand_client;                     // Client for hand control service
+        ros::ServiceClient joint_client;                    // Client for joint control service
+        ros::ServiceClient pose_client;                     // Client for pose control service
+        ros::ServiceClient slerp_client;                    // Client for slerp control service
+	
+};
---
+++ src/PandaSoftHandClient.cpp
@@ -0,0 +1,89 @@
+/* PANDA SOFTHAND CLIENT - Contains all necessary objects and functions to call the services to 
+control Panda + SoftHand
+Authors: George Jose Pollayil - Mathew Jose Pollayil
+Email: gpollayil@gmail.com, mathewjosepollayil@gmail.com  */
+
+#include "PandaSoftHandClient.h"
+
+PandaSoftHandClient::PandaSoftHandClient(ros::NodeHandle& nh_){
+
+    // Initializing node handle
+    this->nh = nh_;
+
+    // Initializing the service names (TODO: Change the hard coded names of the services and parse them from same yaml file of main_server)
+    this->adaptive_service_name = "adaptive_control_service";
+    this->hand_service_name = "hand_control_service";
+    this->joint_service_name = "joint_control_service";
+    this->pose_service_name = "pose_control_service";
+    this->slerp_service_name = "slerp_control_service";
+
+    // Initializing service clients after waiting
+    ros::service::waitForService(this->adaptive_service_name, ros::Duration(1.0));
+    this->adaptive_client = this->nh.serviceClient<adaptive_grasp_controller::adaptive_control>(this->adaptive_service_name);
+
+    ros::service::waitForService(this->hand_service_name, ros::Duration(1.0));
+    this->hand_client = this->nh.serviceClient<adaptive_grasp_controller::hand_control>(this->hand_service_name);
+
+    ros::service::waitForService(this->joint_service_name, ros::Duration(1.0));
+    this->joint_client = this->nh.serviceClient<adaptive_grasp_controller::joint_control>(this->joint_service_name);
+
+    ros::service::waitForService(this->pose_service_name, ros::Duration(1.0));
+    this->pose_client = this->nh.serviceClient<adaptive_grasp_controller::pose_control>(this->pose_service_name);
+
+    ros::service::waitForService(this->slerp_service_name, ros::Duration(1.0));
+    this->slerp_client = this->nh.serviceClient<adaptive_grasp_controller::slerp_control>(this->slerp_service_name);
+}
+
+PandaSoftHandClient::~PandaSoftHandClient(){
+    
+    // Nothing to do here yet
+}
+
+// Service call function for adaptive control
+bool PandaSoftHandClient::call_adaptive_service(bool run){
+
+    // Creating and filling up the request
+    adaptive_grasp_controller::adaptive_control adaptive_control_srv;
+    adaptive_control_srv.request.run = run;
+
+    // Calling the service
+    if(!this->adaptive_client.call(adaptive_control_srv)){
+        ROS_ERROR("Failed to contact the adaptive control server. Returning...");
+        return false;
+    }
+
+    return adaptive_control_srv.response.answer;
+}
+
+// Service call function for hand control
+bool PandaSoftHandClient::call_hand_service(double goal_syn, double goal_duration){
+
+    // Creating and filling up the request
+    adaptive_grasp_controller::hand_control hand_control_srv;
+    hand_control_srv.request.goal_syn = goal_syn;
+    hand_control_srv.request.goal_duration = goal_duration;
+
+    // Calling the service
+    if(!this->hand_client.call(hand_control_srv)){
+        ROS_ERROR("Failed to contact the hand control server. Returning...");
+        return false;
+    }
+
+    return hand_control_srv.response.answer;
+}
+
+// Service call function for joint control
+bool PandaSoftHandClient::call_joint_service(std::vector<double> joint_goal){
+
+    // Creating and filling up the request
+    adaptive_grasp_controller::joint_control joint_control_srv;
+    joint_control_srv.request.joint_goal = joint_goal;
+
+    // Calling the service
+    if(!this->joint_client.call(joint_control_srv)){
+        ROS_ERROR("Failed to contact the joint control server. Returning...");
+        return false;
+    }
+
+    return joint_control_srv.response.answer;
+}
